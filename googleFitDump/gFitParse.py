c28495a4a1b71c9bd83addcd0bd3186b84085b99
Ankan <ankangnv@gmail.com>
2016-07-20 00:27:05 -0700

    gFitData Parser ready but original format of googleFit_Dump.csv is unreliable. Need to confirm

--- googleFitDump/gFitParse.py
+++ googleFitDump/gFitParse.py
@@ -2,3 +2,20 @@
 
 import sys
 
+def ReadBackEachLine(oneLine):
+    text = oneLine.split()
+    fo = open('gParsedFile.csv','a')
+    for i in range(len(text)):
+        fo.write('%s\t' %(text[i]))
+    fo.write("\n")
+    fo.close()
+
+def main():
+    SourceFilename = 'googleFit_Dump.csv'
+    fileReadObj = open(SourceFilename, 'r')
+    for line in fileReadObj:
+        ReadBackEachLine(line)
+    fileReadObj.close()
+
+if __name__ == '__main__':
+  main()
